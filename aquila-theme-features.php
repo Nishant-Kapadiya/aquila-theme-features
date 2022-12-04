3aea99575dcddd4a816438b0b74a752339db04bf
Nishant-Kapadiya <nishantkapadiya8@gmail.com>
2022-12-04 19:13:56 +0530

    Create File

---
+++ aquila-theme-features.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Aquila Theme Features
+ *
+ * @package           aquilaFeatures
+ * @author            Nishant Kapadiya
+ * @copyright         2022 Nishant Kapadiya
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Aquila Theme Features
+ * Description:       Add Gutenberg Blocks.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Nishant Kapadiya
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       fse-features
+ * Domain Path:       /languages
+ */
+/*
+Aquila Theme Features is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Aquila Theme Features is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Aquila Theme Features. If not, see https://wordpress.org/plugin
+*/
